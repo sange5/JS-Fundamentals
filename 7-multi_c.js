481a50f049c428860136f70c87955715d0289667
sange5 <163669819+sange5@users.noreply.github.com>
2025-07-12 13:06:41 +0300

    Create 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,7 @@
+const times = Number.parseInt(process.argv[2]);
+
+if (Number.isNaN(times)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < times; i++) console.log("C is fun");
+}
